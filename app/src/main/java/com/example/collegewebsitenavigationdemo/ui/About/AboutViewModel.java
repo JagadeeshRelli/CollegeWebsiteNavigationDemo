934bcced65fd12cdb0050ad04727c23b6d8d033a
RJ535315 <jagadeeshrelli5353@gmail.com>
2020-09-10 14:42:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/collegewebsitenavigationdemo/ui/About/AboutViewModel.java
@@ -0,0 +1,7 @@
+package com.example.collegewebsitenavigationdemo.ui.About;
+
+import androidx.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
